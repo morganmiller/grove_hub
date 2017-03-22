b84535e0411c1e02234539b24b4bfb880af9dc25
Morgan Miller <m.miller722@gmail.com>
2017-03-22 12:12:50 -0600

    gets facebook auth working without throwing xml error by removing turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,5 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "dashboard#index"
   end
-  # 
-  # match "*path", :to => proc {|env| [200, {
-  #   'Access-Control-Allow-Origin' => '*',
-  #   'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-  #   'Access-Control-Allow-Credentials' => 'true',
-  #   'Access-Control-Request-Method' => '*',
-  #   'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  #   'Content-Type' => 'text/plain'
-  #
-  # }, ["CORS Preflight"]] }, :via => [:options]
+
 end
