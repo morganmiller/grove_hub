7c82fcd8126334281f6df4d5960fece82f063755
Morgan Miller <m.miller722@gmail.com>
2017-03-22 16:21:51 -0600

    adds scope for categorizing actions

--- app/models/action_item.rb
+++ app/models/action_item.rb
@@ -3,4 +3,7 @@ class ActionItem < ApplicationRecord
   validates :category, presence: true
 
   validates :description, presence: true
+
+  scope :category, -> (category_name) { joins(:category).where(categories: {name: category_name}) }
+  
 end
--- spec/models/action_item_spec.rb
+++ spec/models/action_item_spec.rb
@@ -9,5 +9,14 @@ RSpec.describe ActionItem, type: :model do
   it { should be_valid }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:category) }
-  
+
+  describe '#category' do
+    before { @action_item = FactoryGirl.create(:action_item) }
+
+    it 'filters action items by category name' do
+      categorized_actions = ActionItem.category(@action_item.category.name)
+      expect(categorized_actions).to include(@action_item)
+    end
+  end
+
 end
