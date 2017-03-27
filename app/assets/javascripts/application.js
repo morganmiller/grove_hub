67815679313bcae9f0d537628c1cac782937c8f7
Morgan Miller <m.miller722@gmail.com>
2017-03-27 13:44:11 -0600

    fixes materialize select bug and gives html_options to custom link helper

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,15 @@
 
 
 function remove_fields(link) {
-$(link).prev("input[type=hidden]").val("1");
-$(link).closest(".fields").hide(); }
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
+}
 
-function add_fields(link, association, content) { var new_id = new Date().getTime(); var regexp = new RegExp("new_" + association, "g"); $(link).parent().before(content.replace(regexp, new_id)); }
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime(); var regexp = new RegExp("new_" + association, "g"); $(link).parent().before(content.replace(regexp, new_id));
+  $('select').material_select();
+}
 
 $(document).ready(function() {
-    $('select').material_select();
+  $('select').material_select();
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,13 +10,13 @@ module ApplicationHelper
     content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
   end
 
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, html_options)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
     fields = format_form_params(escape_javascript(fields), association.to_s)
-    link_to_function(name, "add_fields(this, \"#{association}\", \"#{fields}\")")
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{fields}\")", html_options)
   end
 
   def format_form_params(fields, association)
