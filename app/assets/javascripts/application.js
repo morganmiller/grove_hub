6d72022864d1b85cdd8a96647a2610841df1de7e
Morgan Miller <m.miller722@gmail.com>
2017-03-27 16:48:30 -0600

    adds line breaks to javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,9 @@ function remove_fields(link) {
 }
 
 function add_fields(link, association, content) {
-  var new_id = new Date().getTime(); var regexp = new RegExp("new_" + association, "g"); $(link).parent().before(content.replace(regexp, new_id));
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g");
+  $(link).parent().before(content.replace(regexp, new_id));
   $('select').material_select();
 }
 
