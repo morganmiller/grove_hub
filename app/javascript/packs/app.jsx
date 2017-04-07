43ce7417c8d2d7efebd0f88835ed7b4a8c32f92a
Morgan Miller <m.miller722@gmail.com>
2017-04-07 12:53:13 -0600

    removes comments

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,6 +1,3 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ApolloClient from 'apollo-client'
@@ -27,8 +24,3 @@ const Root = () => {
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render( <Root />, document.querySelector('#root'))
 })
-
-
-{/* <BrowserRouter>
-  <Match exactly pattern='/' component={App} />
-</BrowserRouter> */}
