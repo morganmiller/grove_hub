4b2cc3636477ddae8de26e31724c2b7a3576e450
Morgan Miller <m.miller722@gmail.com>
2017-04-11 11:15:20 -0600

    adds guiding comments for future architecture

--- app/graphql/mutations/login_mutation.rb
+++ app/graphql/mutations/login_mutation.rb
@@ -10,6 +10,10 @@ LoginMutation = GraphQL::Relay::Mutation.define do
     user = User.find_by(email: args["email"])
     if user && user.valid_password?(args["password"])
       return {user: user}
+      # Needs some sort of auth middleware helpers to create sessions through graphql,
+      # and a way to selectively bypass user authentication for singular graphql endpoint.
+      # (can't be before_action, needs to be cooked into middleware).
+      # Devise may not be best option.
     end
   }
 end
